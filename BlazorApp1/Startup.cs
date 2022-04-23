ff988128534dcbffacd392564237e0a0fcccb9c9
lusalamark <lusalamark63@gmail.com>
2022-04-23 18:29:32 +0300

    Added Pages for different Modules

--- BlazorApp1/Startup.cs
+++ BlazorApp1/Startup.cs
@@ -1,4 +1,4 @@
-using BlazorApp1.Data;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +28,7 @@ namespace BlazorApp1
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+           // services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
